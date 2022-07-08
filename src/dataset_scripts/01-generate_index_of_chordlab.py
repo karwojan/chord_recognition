e262d511e71cd272de3431369f305e9b27b5a1d2
Jan Karwowski <karwojan@tlen.pl>
2022-07-09 00:18:42 +0200

    Implement and apply duplicate filter in chordlab index

--- src/dataset_scripts/01-generate_index_of_chordlab.py
+++ src/dataset_scripts/01-generate_index_of_chordlab.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from glob import glob
 
+# source - rs200
 rs200_original = pd.read_csv("./data/chordlab/rs200/rs200.txt", delimiter="\t", header=None, names=["filepath", "song_rank", "song", "artist", "year", "original"])
 rs200 = pd.DataFrame()
 rs200["filepath"] = "./data/chordlab/rs200/" + rs200_original["filepath"] + "_dt.clt"
@@ -9,6 +10,7 @@ rs200["artist"] = rs200_original["artist"]
 rs200["subset"] = "rs200"
 rs200 = rs200.dropna(axis="index")
 
+# source - rwc pop
 rwc_pop_original = pd.read_csv("./data/chordlab/rwc_pop/rwc_pop.txt", delimiter="\t")
 rwc_pop = pd.DataFrame()
 rwc_pop["filepath"] = rwc_pop_original.apply(lambda x: f"./data/chordlab/rwc_pop/N{int(x.iloc[0].split(' ')[1]):03}-{x.iloc[1]}-T{x.iloc[2].split(' ')[1]}.lab", axis=1)
@@ -17,6 +19,7 @@ rwc_pop["artist"] = rwc_pop_original["Artist (Vocal)"]
 rwc_pop["subset"] = "rwc_pop"
 rwc_pop = rwc_pop.dropna(axis="index")
 
+# source - isophonics
 isophonics = pd.DataFrame()
 isophonics["filepath"] = glob("./data/chordlab/isophonics/**/*.lab", recursive=True)
 isophonics["song"] = isophonics["filepath"].str.split("/", expand=True)[6]
@@ -30,6 +33,7 @@ isophonics["artist"] = isophonics["filepath"].str.split("/", expand=True)[4]
 isophonics["subset"] = "isophonics"
 isophonics = isophonics.dropna(axis="index")
 
+# source - billboard
 billboard_ids = {int(filename.split("/")[4]) for filename in glob("./data/chordlab/mcgill_billboard/**/*.lab", recursive=True)}
 billboard_original = pd.read_csv("./data/chordlab/mcgill_billboard/billboard-2.0-index.csv").filter(billboard_ids, axis="index")
 billboard = pd.DataFrame()
@@ -39,6 +43,7 @@ billboard["artist"] = billboard_original["artist"]
 billboard["subset"] = "billboard"
 billboard = billboard.dropna(axis="index")
 
+# source - uspop
 uspop = pd.DataFrame()
 uspop["filepath"] = glob("./data/chordlab/uspop/**/*.lab", recursive=True)
 uspop["song"] = uspop["filepath"].str.split("/", expand=True)[6]
@@ -51,6 +56,7 @@ uspop["artist"] = uspop["filepath"].str.split("/", expand=True)[4]
 uspop["artist"] = uspop["artist"].str.replace(r"_", " ", regex=True)
 uspop["subset"] = "uspop"
 
+# source - robbie williams
 robbie = pd.DataFrame()
 robbie["filepath"] = glob("./data/chordlab/robbie_williams/**/*.txt", recursive=True)
 robbie["song"] = robbie["filepath"].str.split("/", expand=True)[5]
@@ -61,5 +67,20 @@ robbie["album"] = robbie["album"].str.replace(r"\d{4}-", "", regex=True)
 robbie["artist"] = "Robbie Williams"
 robbie["subset"] = "robbie_williams"
 
-compound = pd.concat([rs200, rwc_pop, isophonics, billboard, uspop, robbie])
+# concatenate to single index
+compound = pd.concat([rs200, rwc_pop, isophonics, billboard, uspop, robbie]).reset_index(drop=True)
+
+# filter duplicates
+compound["_song"] = compound["song"].str.lower().str.strip()
+compound["_artist"] = compound["artist"].str.lower().str.strip()
+compound["_album"] = compound["album"].str.lower().str.strip()
+for song_and_artist, probably_same_songs in compound.groupby(["_song", "_artist"]):
+    for album, really_same_songs in probably_same_songs.groupby("album", dropna=False):
+        if not isinstance(album, str) and not probably_same_songs["album"].isna().all():
+            compound.drop(really_same_songs.index, inplace=True)
+        else:
+            compound.drop(really_same_songs.iloc[1:].index, inplace=True)
+compound.drop(["_song", "_artist", "_album"], axis=1, inplace=True)
+
+# save to file
 compound.to_csv("./data/index.csv", sep=";", header=True, index=False)
