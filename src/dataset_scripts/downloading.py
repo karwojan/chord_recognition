f97dc12c37c0ee38894c738c1cf6693651c3ee8e
Jan Karwowski <j.karwowski@pixel.com.pl>
2022-08-17 12:11:12 +0200

    Little fixes in audio downloading logic

--- src/dataset_scripts/downloading.py
+++ src/dataset_scripts/downloading.py
@@ -3,12 +3,12 @@ import pandas as pd
 import pytube
 from pytube import YouTube
 from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor
 from glob import glob
 
 
 def download_video(video_id: str, output_path: str) -> str:
-    if len(glob(os.path.join(output_path, f"{video_id}_*"))) == 0:
+    paths = glob(os.path.join(output_path, f"{video_id}_*"))
+    if len(paths) == 0:
         try:
             return (
                 YouTube(f"http://youtube.com/watch?v={video_id}")
@@ -18,6 +18,8 @@ def download_video(video_id: str, output_path: str) -> str:
                 .first()
                 .download(output_path, filename_prefix=f"{video_id}_")
             )
-        except pytube.exceptions.VideoUnavailable:
-            print(f"Video {video_id} is unavailable!")
+        except Exception as e:
+            print(f"Video {video_id} is unavailable:", e)
             return None
+    else:
+        return paths[0]
