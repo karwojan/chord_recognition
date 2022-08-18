4738ce24a3316a95c31974dcbb9de87f24c99ee2
Jan Karwowski <j.karwowski@pixel.com.pl>
2022-08-18 23:17:33 +0200

    Adjust parameters in select_proposals_and_split script

--- src/dataset_scripts/03-select_proposals_and_split.py
+++ src/dataset_scripts/03-select_proposals_and_split.py
@@ -2,13 +2,13 @@ import pandas as pd
 import numpy as np
 
 test_fraction = 0.1
-validation_fraction = 0.05
+validation_fraction = 0.1
 
 # load index
 index = pd.read_csv("./data/index.csv", sep=";")
 
 # select songs with "properly matched" videos
-filtered_index = index.query("csr > 0.3 and abs(start_diff) < 2 and abs(stop_diff) < 5")
+filtered_index = index.query("csr > 0.3 and abs(stop_diff) < 10")
 
 # split to train/test
 for subset in filtered_index.subset.unique():
