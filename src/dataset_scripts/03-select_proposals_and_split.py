b660cde19703e22a4bfb802527d326e18f95aee3
Jan Karwowski <j.karwowski@pixel.com.pl>
2022-08-19 12:38:24 +0200

    Filter duplicated video_ids during proposal selection + generated new index

--- src/dataset_scripts/03-select_proposals_and_split.py
+++ src/dataset_scripts/03-select_proposals_and_split.py
@@ -10,7 +10,14 @@ index = pd.read_csv("./data/index.csv", sep=";")
 # select songs with "properly matched" videos
 filtered_index = index.query("csr > 0.3 and abs(stop_diff) < 10")
 
-# split to train/test
+# filter songs with repeated video_id
+repeated_video_ids = filtered_index.groupby("video_id").count().query("filepath > 1").reset_index()["video_id"]
+for repeated_video_id in repeated_video_ids:
+    repeated_songs = filtered_index.query("video_id == @repeated_video_id")
+    filtered_index = filtered_index.drop(index=repeated_songs.sort_values(by="csr").iloc[:-1].index)
+
+# split to train/test (not empty purpose mark song as selected - with properly matched, unique video)
+index["purpose"] = None
 for subset in filtered_index.subset.unique():
     subset_indices = np.random.permutation(np.array(filtered_index.query("subset == @subset").index))
     n_test_items = int(np.ceil(len(subset_indices) * test_fraction))
