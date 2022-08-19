8b5c9a79bc7e108431931c614af7a1b0a752ce3c
Jan Karwowski <j.karwowski@pixel.com.pl>
2022-08-19 11:19:00 +0200

    Add BTC model (original implementation) and BTC training script

---
+++ src/training/btc_model.py
@@ -0,0 +1,496 @@
+""" This is implementation from https://github.com/jayg996/BTC-ISMIR19 """
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import numpy as np
+
+use_cuda = torch.cuda.is_available()
+
+
+def _gen_bias_mask(max_length):
+    """
+    Generates bias values (-Inf) to mask future timesteps during attention
+    """
+    np_mask = np.triu(np.full([max_length, max_length], -np.inf), 1)
+    torch_mask = torch.from_numpy(np_mask).type(torch.FloatTensor)
+    return torch_mask.unsqueeze(0).unsqueeze(1)
+
+
+def _gen_timing_signal(length, channels, min_timescale=1.0, max_timescale=1.0e4):
+    """
+    Generates a [1, length, channels] timing signal consisting of sinusoids
+    Adapted from:
+    https://github.com/tensorflow/tensor2tensor/blob/master/tensor2tensor/layers/common_attention.py
+    """
+    position = np.arange(length)
+    num_timescales = channels // 2
+    log_timescale_increment = (
+            math.log(float(max_timescale) / float(min_timescale)) /
+            (float(num_timescales) - 1))
+    inv_timescales = min_timescale * np.exp(
+        np.arange(num_timescales).astype(float) * -log_timescale_increment)
+    scaled_time = np.expand_dims(position, 1) * np.expand_dims(inv_timescales, 0)
+
+    signal = np.concatenate([np.sin(scaled_time), np.cos(scaled_time)], axis=1)
+    signal = np.pad(signal, [[0, 0], [0, channels % 2]],
+                    'constant', constant_values=[0.0, 0.0])
+    signal = signal.reshape([1, length, channels])
+
+    return torch.from_numpy(signal).type(torch.FloatTensor)
+
+
+class LayerNorm(nn.Module):
+    # Borrowed from jekbradbury
+    # https://github.com/pytorch/pytorch/issues/1959
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.gamma = nn.Parameter(torch.ones(features))
+        self.beta = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
+
+
+class OutputLayer(nn.Module):
+    """
+    Abstract base class for output layer.
+    Handles projection to output labels
+    """
+    def __init__(self, hidden_size, output_size, probs_out=False):
+        super(OutputLayer, self).__init__()
+        self.output_size = output_size
+        self.output_projection = nn.Linear(hidden_size, output_size)
+        self.probs_out = probs_out
+        self.lstm = nn.LSTM(input_size=hidden_size, hidden_size=int(hidden_size/2), batch_first=True, bidirectional=True)
+        self.hidden_size = hidden_size
+
+    def loss(self, hidden, labels):
+        raise NotImplementedError('Must implement {}.loss'.format(self.__class__.__name__))
+
+
+class SoftmaxOutputLayer(OutputLayer):
+    """
+    Implements a softmax based output layer
+    """
+    def forward(self, hidden):
+        logits = self.output_projection(hidden)
+        probs = F.softmax(logits, -1)
+        # _, predictions = torch.max(probs, dim=-1)
+        topk, indices = torch.topk(probs, 2)
+        predictions = indices[:, :, 0]
+        second = indices[:, :, 1]
+        if self.probs_out is True:
+            return logits
+            # return probs
+        return predictions, second
+
+    def loss(self, hidden, labels):
+        logits = self.output_projection(hidden)
+        log_probs = F.log_softmax(logits, -1)
+        return F.nll_loss(log_probs.view(-1, self.output_size), labels.view(-1))
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    Multi-head attention as per https://arxiv.org/pdf/1706.03762.pdf
+    Refer Figure 2
+    """
+
+    def __init__(self, input_depth, total_key_depth, total_value_depth, output_depth,
+                 num_heads, bias_mask=None, dropout=0.0, attention_map=False):
+        """
+        Parameters:
+            input_depth: Size of last dimension of input
+            total_key_depth: Size of last dimension of keys. Must be divisible by num_head
+            total_value_depth: Size of last dimension of values. Must be divisible by num_head
+            output_depth: Size last dimension of the final output
+            num_heads: Number of attention heads
+            bias_mask: Masking tensor to prevent connections to future elements
+            dropout: Dropout probability (Should be non-zero only during training)
+        """
+        super(MultiHeadAttention, self).__init__()
+        # Checks borrowed from
+        # https://github.com/tensorflow/tensor2tensor/blob/master/tensor2tensor/layers/common_attention.py
+        if total_key_depth % num_heads != 0:
+            raise ValueError("Key depth (%d) must be divisible by the number of "
+                             "attention heads (%d)." % (total_key_depth, num_heads))
+        if total_value_depth % num_heads != 0:
+            raise ValueError("Value depth (%d) must be divisible by the number of "
+                             "attention heads (%d)." % (total_value_depth, num_heads))
+
+        self.attention_map = attention_map
+
+        self.num_heads = num_heads
+        self.query_scale = (total_key_depth // num_heads) ** -0.5
+        self.bias_mask = bias_mask
+
+        # Key and query depth will be same
+        self.query_linear = nn.Linear(input_depth, total_key_depth, bias=False)
+        self.key_linear = nn.Linear(input_depth, total_key_depth, bias=False)
+        self.value_linear = nn.Linear(input_depth, total_value_depth, bias=False)
+        self.output_linear = nn.Linear(total_value_depth, output_depth, bias=False)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def _split_heads(self, x):
+        """
+        Split x such to add an extra num_heads dimension
+        Input:
+            x: a Tensor with shape [batch_size, seq_length, depth]
+        Returns:
+            A Tensor with shape [batch_size, num_heads, seq_length, depth/num_heads]
+        """
+        if len(x.shape) != 3:
+            raise ValueError("x must have rank 3")
+        shape = x.shape
+        return x.view(shape[0], shape[1], self.num_heads, shape[2] // self.num_heads).permute(0, 2, 1, 3)
+
+    def _merge_heads(self, x):
+        """
+        Merge the extra num_heads into the last dimension
+        Input:
+            x: a Tensor with shape [batch_size, num_heads, seq_length, depth/num_heads]
+        Returns:
+            A Tensor with shape [batch_size, seq_length, depth]
+        """
+        if len(x.shape) != 4:
+            raise ValueError("x must have rank 4")
+        shape = x.shape
+        return x.permute(0, 2, 1, 3).contiguous().view(shape[0], shape[2], shape[3] * self.num_heads)
+
+    def forward(self, queries, keys, values):
+
+        # Do a linear for each component
+        queries = self.query_linear(queries)
+        keys = self.key_linear(keys)
+        values = self.value_linear(values)
+
+        # Split into multiple heads
+        queries = self._split_heads(queries)
+        keys = self._split_heads(keys)
+        values = self._split_heads(values)
+
+        # Scale queries
+        queries *= self.query_scale
+
+        # Combine queries and keys
+        logits = torch.matmul(queries, keys.permute(0, 1, 3, 2))
+
+        # Add bias to mask future values
+        if self.bias_mask is not None:
+            logits += self.bias_mask[:, :, :logits.shape[-2], :logits.shape[-1]].type_as(logits.data)
+
+        # Convert to probabilites
+        weights = nn.functional.softmax(logits, dim=-1)
+
+        # Dropout
+        weights = self.dropout(weights)
+
+        # Combine with values to get context
+        contexts = torch.matmul(weights, values)
+
+        # Merge heads
+        contexts = self._merge_heads(contexts)
+        # contexts = torch.tanh(contexts)
+
+        # Linear to get output
+        outputs = self.output_linear(contexts)
+
+        if self.attention_map is True:
+            return outputs, weights
+
+        return outputs
+
+
+class Conv(nn.Module):
+    """
+    Convenience class that does padding and convolution for inputs in the format
+    [batch_size, sequence length, hidden size]
+    """
+
+    def __init__(self, input_size, output_size, kernel_size, pad_type):
+        """
+        Parameters:
+            input_size: Input feature size
+            output_size: Output feature size
+            kernel_size: Kernel width
+            pad_type: left -> pad on the left side (to mask future data_loader),
+                      both -> pad on both sides
+        """
+        super(Conv, self).__init__()
+        padding = (kernel_size - 1, 0) if pad_type == 'left' else (kernel_size // 2, (kernel_size - 1) // 2)
+        self.pad = nn.ConstantPad1d(padding, 0)
+        self.conv = nn.Conv1d(input_size, output_size, kernel_size=kernel_size, padding=0)
+
+    def forward(self, inputs):
+        inputs = self.pad(inputs.permute(0, 2, 1))
+        outputs = self.conv(inputs).permute(0, 2, 1)
+
+        return outputs
+
+
+class PositionwiseFeedForward(nn.Module):
+    """
+    Does a Linear + RELU + Linear on each of the timesteps
+    """
+
+    def __init__(self, input_depth, filter_size, output_depth, layer_config='ll', padding='left', dropout=0.0):
+        """
+        Parameters:
+            input_depth: Size of last dimension of input
+            filter_size: Hidden size of the middle layer
+            output_depth: Size last dimension of the final output
+            layer_config: ll -> linear + ReLU + linear
+                          cc -> conv + ReLU + conv etc.
+            padding: left -> pad on the left side (to mask future data_loader),
+                     both -> pad on both sides
+            dropout: Dropout probability (Should be non-zero only during training)
+        """
+        super(PositionwiseFeedForward, self).__init__()
+
+        layers = []
+        sizes = ([(input_depth, filter_size)] +
+                 [(filter_size, filter_size)] * (len(layer_config) - 2) +
+                 [(filter_size, output_depth)])
+
+        for lc, s in zip(list(layer_config), sizes):
+            if lc == 'l':
+                layers.append(nn.Linear(*s))
+            elif lc == 'c':
+                layers.append(Conv(*s, kernel_size=3, pad_type=padding))
+            else:
+                raise ValueError("Unknown layer type {}".format(lc))
+
+        self.layers = nn.ModuleList(layers)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, inputs):
+        x = inputs
+        for i, layer in enumerate(self.layers):
+            x = layer(x)
+            if i < len(self.layers):
+                x = self.relu(x)
+                x = self.dropout(x)
+
+        return x
+
+
+class self_attention_block(nn.Module):
+    def __init__(self, hidden_size, total_key_depth, total_value_depth, filter_size, num_heads,
+                 bias_mask=None, layer_dropout=0.0, attention_dropout=0.0, relu_dropout=0.0, attention_map=False):
+        super(self_attention_block, self).__init__()
+
+        self.attention_map = attention_map
+        self.multi_head_attention = MultiHeadAttention(hidden_size, total_key_depth, total_value_depth, hidden_size, num_heads, bias_mask, attention_dropout, attention_map)
+        self.positionwise_convolution = PositionwiseFeedForward(hidden_size, filter_size, hidden_size, layer_config='cc', padding='both', dropout=relu_dropout)
+        self.dropout = nn.Dropout(layer_dropout)
+        self.layer_norm_mha = LayerNorm(hidden_size)
+        self.layer_norm_ffn = LayerNorm(hidden_size)
+
+    def forward(self, inputs):
+        x = inputs
+
+        # Layer Normalization
+        x_norm = self.layer_norm_mha(x)
+
+        # Multi-head attention
+        if self.attention_map is True:
+            y, weights = self.multi_head_attention(x_norm, x_norm, x_norm)
+        else:
+            y = self.multi_head_attention(x_norm, x_norm, x_norm)
+
+        # Dropout and residual
+        x = self.dropout(x + y)
+
+        # Layer Normalization
+        x_norm = self.layer_norm_ffn(x)
+
+        # Positionwise Feedforward
+        y = self.positionwise_convolution(x_norm)
+
+        # Dropout and residual
+        y = self.dropout(x + y)
+
+        if self.attention_map is True:
+            return y, weights
+        return y
+
+
+class bi_directional_self_attention(nn.Module):
+    def __init__(self, hidden_size, total_key_depth, total_value_depth, filter_size, num_heads, max_length,
+                 layer_dropout=0.0, attention_dropout=0.0, relu_dropout=0.0):
+
+        super(bi_directional_self_attention, self).__init__()
+
+        self.weights_list = list()
+
+        params = (hidden_size,
+                  total_key_depth or hidden_size,
+                  total_value_depth or hidden_size,
+                  filter_size,
+                  num_heads,
+                  _gen_bias_mask(max_length),
+                  layer_dropout,
+                  attention_dropout,
+                  relu_dropout,
+                  True)
+
+        self.attn_block = self_attention_block(*params)
+
+        params = (hidden_size,
+                  total_key_depth or hidden_size,
+                  total_value_depth or hidden_size,
+                  filter_size,
+                  num_heads,
+                  torch.transpose(_gen_bias_mask(max_length), dim0=2, dim1=3),
+                  layer_dropout,
+                  attention_dropout,
+                  relu_dropout,
+                  True)
+
+        self.backward_attn_block = self_attention_block(*params)
+
+        self.linear = nn.Linear(hidden_size*2, hidden_size)
+
+    def forward(self, inputs):
+        x, list = inputs
+
+        # Forward Self-attention Block
+        encoder_outputs, weights = self.attn_block(x)
+        # Backward Self-attention Block
+        reverse_outputs, reverse_weights = self.backward_attn_block(x)
+        # Concatenation and Fully-connected Layer
+        outputs = torch.cat((encoder_outputs, reverse_outputs), dim=2)
+        y = self.linear(outputs)
+
+        # Attention weights for Visualization
+        self.weights_list = list
+        self.weights_list.append(weights)
+        self.weights_list.append(reverse_weights)
+        return y, self.weights_list
+
+
+class bi_directional_self_attention_layers(nn.Module):
+    def __init__(self, embedding_size, hidden_size, num_layers, num_heads, total_key_depth, total_value_depth,
+                 filter_size, max_length=100, input_dropout=0.0, layer_dropout=0.0,
+                 attention_dropout=0.0, relu_dropout=0.0):
+        super(bi_directional_self_attention_layers, self).__init__()
+
+        self.timing_signal = _gen_timing_signal(max_length, hidden_size)
+        params = (hidden_size,
+                  total_key_depth or hidden_size,
+                  total_value_depth or hidden_size,
+                  filter_size,
+                  num_heads,
+                  max_length,
+                  layer_dropout,
+                  attention_dropout,
+                  relu_dropout)
+        self.embedding_proj = nn.Linear(embedding_size, hidden_size, bias=False)
+        self.self_attn_layers = nn.Sequential(*[bi_directional_self_attention(*params) for l in range(num_layers)])
+        self.layer_norm = LayerNorm(hidden_size)
+        self.input_dropout = nn.Dropout(input_dropout)
+
+    def forward(self, inputs):
+        # Add input dropout
+        x = self.input_dropout(inputs)
+
+        # Project to hidden size
+        x = self.embedding_proj(x)
+
+        # Add timing signal
+        x += self.timing_signal[:, :inputs.shape[1], :].type_as(inputs.data)
+
+        # A Stack of Bi-directional Self-attention Layers
+        y, weights_list = self.self_attn_layers((x, []))
+
+        # Layer Normalization
+        y = self.layer_norm(y)
+        return y, weights_list
+
+
+class BTC_model(nn.Module):
+    def __init__(self, config):
+        super(BTC_model, self).__init__()
+
+        self.timestep = config['timestep']
+        self.probs_out = config['probs_out']
+
+        params = (config['feature_size'],
+                  config['hidden_size'],
+                  config['num_layers'],
+                  config['num_heads'],
+                  config['total_key_depth'],
+                  config['total_value_depth'],
+                  config['filter_size'],
+                  config['timestep'],
+                  config['input_dropout'],
+                  config['layer_dropout'],
+                  config['attention_dropout'],
+                  config['relu_dropout'])
+
+        self.self_attn_layers = bi_directional_self_attention_layers(*params)
+        self.output_layer = SoftmaxOutputLayer(hidden_size=config['hidden_size'], output_size=config['num_chords'], probs_out=config['probs_out'])
+
+    def forward(self, x, labels):
+        labels = labels.view(-1, self.timestep)
+        # Output of Bi-directional Self-attention Layers
+        self_attn_output, weights_list = self.self_attn_layers(x)
+
+        # return logit values for CRF
+        if self.probs_out is True:
+            logits = self.output_layer(self_attn_output)
+            return logits
+
+        # Output layer and Soft-max
+        prediction, second = self.output_layer(self_attn_output)
+        prediction = prediction.view(-1)
+        second = second.view(-1)
+
+        # Loss Calculation
+        loss = self.output_layer.loss(self_attn_output, labels)
+        return prediction, loss, weights_list, second
+
+
+if __name__ == "__main__":
+    from torchinfo import summary
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    batch_size = 2
+    timestep = 108
+    feature_size = 144
+    num_chords = 25
+
+    features = torch.randn(batch_size, timestep, feature_size, requires_grad=True).to(device)
+    chords = torch.randint(25, (batch_size*timestep,)).to(device)
+    print(features.shape)
+    print(chords.shape)
+
+    model = BTC_model({
+        "feature_size": 144,
+        "timestep": 108,
+        "num_chords": 25,
+        "input_dropout": 0.2,
+        "layer_dropout": 0.2,
+        "attention_dropout": 0.2,
+        "relu_dropout": 0.2,
+        "num_layers": 8,
+        "num_heads": 4,
+        "hidden_size": 128,
+        "total_key_depth": 128,
+        "total_value_depth": 128,
+        "filter_size": 128,
+        "loss": 'ce',
+        "probs_out": False
+    }).to(device)
+    summary(model, input_data=(features, chords))
+
+    prediction, loss, weights_list, second = model(features, chords)
+    print(prediction.size())
+    print(loss)
---
+++ src/training/btc_training.py
@@ -0,0 +1,121 @@
+import torch
+import torch.distributed as dist
+import argparse
+
+# from torch.nn.parallel import DistributedDataParallel
+from torch.utils.data import DataLoader
+import mlflow
+from tqdm import tqdm
+from einops import rearrange
+
+from src.training.dataset import SongDataset, preprocess_cqt
+from src.training.btc_model import BTC_model
+
+
+def create_argparser():
+    parser = argparse.ArgumentParser()
+
+    # dataset
+    parser.add_argument("--sample_rate", type=int, required=True)
+    parser.add_argument("--frame_size", type=int, required=True)
+    parser.add_argument("--hop_size", type=int, required=True)
+    parser.add_argument("--frames_per_item", type=int, required=True)
+
+    # training
+    parser.add_argument("--n_epochs", type=int, required=True)
+    parser.add_argument("--batch_size", type=int, required=True)
+
+    return parser
+
+
+def train(args):
+    # init torch distributed
+    # dist.init_process_group()
+
+    # init mlflow
+    mlflow.set_experiment("btc_original_implementation")
+    mlflow.log_params(args.__dict__)
+
+    # init datasets and data loaders
+    ds_kwargs = {
+        "sample_rate": args.sample_rate,
+        "frame_size": args.frame_size,
+        "hop_size": args.hop_size,
+        "frames_per_item": args.frames_per_item,
+        "items_per_song_factor": 1 / args.frames_per_item,
+        "audio_preprocessing": preprocess_cqt,
+        "labels_vocabulary": "maj_min",
+    }
+    train_ds = SongDataset("train", **ds_kwargs)
+    train_dl = DataLoader(
+        train_ds, batch_size=args.batch_size, shuffle=True, num_workers=5
+    )
+    validate_ds = SongDataset("validate", **ds_kwargs)
+    validate_dl = DataLoader(
+        validate_ds, batch_size=args.batch_size, shuffle=True, num_workers=5
+    )
+
+    # init model and optimizer
+    btc = BTC_model(
+        {
+            "feature_size": 144,
+            "timestep": args.frames_per_item,
+            "num_chords": 25,
+            "input_dropout": 0.2,
+            "layer_dropout": 0.2,
+            "attention_dropout": 0.2,
+            "relu_dropout": 0.2,
+            "num_layers": 8,
+            "num_heads": 4,
+            "hidden_size": 128,
+            "total_key_depth": 128,
+            "total_value_depth": 128,
+            "filter_size": 128,
+            "loss": "ce",
+            "probs_out": False,
+        }
+    ).cuda()
+    # btc = DistributedDataParallel(btc.cuda())
+    optimizer = torch.optim.AdamW(btc.parameters())
+
+    for epoch in tqdm(range(args.n_epochs), unit="epoch"):
+        # train
+        btc.train()
+        all_predictions = 0
+        all_correct_predictions = 0
+        for audio, labels in tqdm(train_dl, unit="batch", total=len(train_dl)):
+            audio, labels = audio.cuda(), labels.cuda()
+            prediction, loss, weights, second = btc(audio, labels)
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            with torch.no_grad():
+                labels = rearrange(labels, "b c -> (b c)")
+                mlflow.log_metric("train / batch / loss", loss)
+                mlflow.log_metric("train / batch / accuracy", torch.count_nonzero(prediction == labels) / len(prediction))
+
+                all_predictions += len(prediction)
+                all_correct_predictions += torch.count_nonzero(prediction == labels)
+        mlflow.log_metric("train / epoch / accuracy", all_correct_predictions / all_predictions)
+
+        # validate
+        btc.eval()
+        with torch.no_grad():
+            all_predictions = 0
+            all_correct_predictions = 0
+            for audio, labels in tqdm(
+                validate_dl, unit="batch", total=len(validate_dl)
+            ):
+                audio, labels = audio.cuda(), labels.cuda()
+                prediction, loss, weights, second = btc(audio, labels)
+
+                all_predictions += len(prediction)
+                all_correct_predictions += torch.count_nonzero(prediction == rearrange(labels, "b c -> (b c)"))
+        mlflow.log_metric("validate / epoch / accuracy", all_correct_predictions / all_predictions)
+
+
+if __name__ == "__main__":
+    parser = create_argparser()
+    train(parser.parse_args())
