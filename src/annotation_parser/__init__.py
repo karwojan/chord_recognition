610c0320cc24f969f00be816cca8c8d37c3b0ed1
Jan Karwowski <j.karwowski@pixel.com.pl>
2022-08-20 15:29:07 +0200

    Little refactor of annotation_parser.__init__

--- src/annotation_parser/__init__.py
+++ src/annotation_parser/__init__.py
@@ -2,21 +2,29 @@ import lark
 from src.annotation_parser.labfile_transformer import LabFileTransformer
 from src.annotation_parser.rs200_dt_transformer import Rs200DtTransformer
 
-labfile_transformer = LabFileTransformer()
-labfile_parser = lark.Lark.open("./src/annotation_parser/labfile_grammar.lark", parser="lalr", start=["labfile", "chord"])
+labfile_parser = lark.Lark.open(
+    "./src/annotation_parser/labfile_grammar.lark",
+    parser="lalr",
+    start=["labfile", "chord"],
+    transformer=LabFileTransformer(),
+)
 
-rs200_dt_transformer = Rs200DtTransformer()
-rs200_dt_parser = lark.Lark.open("./src/annotation_parser/rs200_dt_grammar.lark", parser="lalr", start=["cltfile", "chord"])
+rs200_dt_parser = lark.Lark.open(
+    "./src/annotation_parser/rs200_dt_grammar.lark",
+    parser="lalr",
+    start=["cltfile", "chord"],
+    transformer=Rs200DtTransformer(),
+)
 
 
 def parse_annotation_file(path: str):
     with open(path) as f:
         text = f.read()
     if path.endswith(".lab") or path.endswith(".txt"):
-        return labfile_transformer.transform(labfile_parser.parse(text, start="labfile"))
+        return labfile_parser.parse(text, start="labfile")
     elif path.endswith("_dt.clt"):
-        return rs200_dt_transformer.transform(rs200_dt_parser.parse(text, start="cltfile"))
+        return rs200_dt_parser.parse(text, start="cltfile")
 
 
 def parse_lab_annotation(annotation: str):
-    return labfile_transformer.transform(labfile_parser.parse(annotation, start="chord"))
+    return labfile_parser.parse(annotation, start="chord")
