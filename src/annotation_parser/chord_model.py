b189227803affbd6e534796c234fde37d0f55744
Jan Karwowski <j.karwowski@pixel.com.pl>
2022-08-20 14:12:50 +0200

    Little refactor of chord_model

--- src/annotation_parser/chord_model.py
+++ src/annotation_parser/chord_model.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Set, List
+from typing import Set, List, Dict
 from dataclasses import dataclass, replace
 import numpy as np
 
@@ -15,6 +15,12 @@ class Chord:
     root: int
     components: Set[Interval]
     bass: Interval
+
+    def shift(self, shift: int) -> Chord:
+        if self.root is None:
+            return self
+        else:
+            return replace(self, root=(self.root + shift) % 12)
 
     def to_label(self, vocabulary_name: str) -> int:
         vocabulary = vocabularies[vocabulary_name]
@@ -48,7 +54,7 @@ class Chord:
             )
 
     @staticmethod
-    def from_label(label: int, vocabulary_name) -> Chord:
+    def from_label(label: int, vocabulary_name: str) -> Chord:
         vocabulary = vocabularies[vocabulary_name]
 
         if label == 0:
@@ -61,20 +67,7 @@ class Chord:
 
         pattern_chord = vocabulary[(label - 1) // 12]
 
-        return Chord(
-            root,
-            {replace(interval) for interval in pattern_chord.components},
-            replace(pattern_chord.bass),
-        )
-
-    def shift(self, shift: int) -> Chord:
-        if self.root is None:
-            return replace(self)
-        return Chord(
-            (self.root + shift) % 12,
-            {replace(interval) for interval in self.components},
-            replace(self.bass),
-        )
+        return replace(pattern_chord, root=root)
 
 
 @dataclass(frozen=True)
@@ -83,7 +76,7 @@ class ChordOccurence:
     stop: float
     chord: Chord
 
-    def to_label_occurence(self, vocabulary_name: str):
+    def to_label_occurence(self, vocabulary_name: str) -> LabelOccurence:
         return LabelOccurence(
             self.start, self.stop, self.chord.to_label(vocabulary_name)
         )
@@ -95,8 +88,13 @@ class LabelOccurence:
     stop: float
     label: int
 
+    def to_chord_occurence(self, vocabulary_name: str) -> ChordOccurence:
+        return ChordOccurence(
+            self.start, self.stop, Chord.from_label(self.label, vocabulary_name)
+        )
 
-vocabularies = {
+
+vocabularies: Dict[str, List[Chord]] = {
     "root_only": [],
     "maj_min": [
         Chord(0, {Interval(1), Interval(3), Interval(5)}, Interval(1)),
@@ -106,15 +104,15 @@ vocabularies = {
 
 
 def csr(reals: List[LabelOccurence], preds: List[LabelOccurence]):
-    reals = [("real", occurence) for occurence in reals]
-    reals.append(("real", LabelOccurence(reals[-1][1].stop, np.inf, np.nan)))
-    preds = [("pred", occurence) for occurence in preds]
-    preds.append(("pred", LabelOccurence(preds[-1][1].stop, np.inf, np.nan)))
-    occurences = list(sorted(reals + preds, key=lambda c: c[1].start))
+    _reals = [("real", occurence) for occurence in reals]
+    _reals.append(("real", LabelOccurence(_reals[-1][1].stop, np.inf, np.nan)))
+    _preds = [("pred", occurence) for occurence in preds]
+    _preds.append(("pred", LabelOccurence(_preds[-1][1].stop, np.inf, np.nan)))
+    occurences = list(sorted(_reals + _preds, key=lambda c: c[1].start))
     last_label = {"real": np.nan, "pred": np.nan}
-    common_label_time = 0
+    common_label_time = 0.0
     for (a_type, a), (b_type, b) in zip(occurences[:-1], occurences[1:]):
         last_label[a_type] = a.label
         if last_label["real"] == last_label["pred"]:
             common_label_time += b.start - a.start
-    return common_label_time / (reals[-1][1].start - reals[0][1].start)
+    return common_label_time / (_reals[-1][1].start - _reals[0][1].start)
