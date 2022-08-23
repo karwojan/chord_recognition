084326aaded1fbf95ca5d0b589c0d5b9abb82948
Jan Karwowski <j.karwowski@pixel.com.pl>
2022-08-23 16:06:35 +0200

    Add my own implementation of BTC model

--- src/training/transformer.py
+++ src/training/transformer.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from einops import rearrange
+from einops import rearrange, repeat
 
 
-def positional_encoding(sequence_length, dim):
+def positional_encoding(sequence_length: int, dim: int) -> torch.Tensor:
     pos = torch.arange(0, sequence_length)
     wavelengths = 10000 ** (torch.linspace(0, dim, int(dim / 2)) / dim)
     args = rearrange(pos, "s -> s 1") / rearrange(wavelengths, "d -> 1 d")
@@ -12,16 +12,17 @@ def positional_encoding(sequence_length, dim):
 
 
 class MultiheadSelfAttention(nn.Module):
-    def __init__(self, n_heads, dim):
+    def __init__(self, dim: int, n_heads: int, mask: str = None):
         super().__init__()
-        self.n_heads = n_heads
         self.dim = dim
+        self.n_heads = n_heads
+        self.mask = mask
         self.qkv = nn.Linear(dim, 3 * dim)
         self.out = nn.Linear(dim, dim)
         self.factor = 1.0 / torch.sqrt(torch.tensor(dim // n_heads))
         assert dim % n_heads == 0
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         # calculate Q, K, V
         Q, K, V = rearrange(
             self.qkv(x),
@@ -30,9 +31,25 @@ class MultiheadSelfAttention(nn.Module):
             head=self.n_heads,
         )
 
+        # optional masking
+        b, s, _ = Q.shape
+        mask = torch.zeros((b, s, s)).to(x.device)
+        if self.mask == "right":
+            mask[
+                :,
+                repeat(torch.arange(s), "s -> S s", S=s)
+                > repeat(torch.arange(s), "s -> s S", S=s),
+            ] = -torch.inf
+        elif self.mask == "left":
+            mask[
+                :,
+                repeat(torch.arange(s), "s -> S s", S=s)
+                < repeat(torch.arange(s), "s -> s S", S=s),
+            ] = -torch.inf
+
         # calculate attention weights
         attention_weights = F.softmax(
-            torch.bmm(Q, rearrange(K, "b s c -> b c s")) * self.factor, dim=-1
+            torch.bmm(Q, rearrange(K, "b s c -> b c s")) * self.factor + mask, dim=-1
         )
 
         # apply attention
@@ -47,23 +64,25 @@ class MultiheadSelfAttention(nn.Module):
 
 
 class TransformerBlock(nn.Module):
-    def __init__(self, n_heads, dim):
+    def __init__(self, dim: int, n_heads: int):
         super().__init__()
-        self.msa = MultiheadSelfAttention(n_heads, dim)
+        self.msa = MultiheadSelfAttention(dim, n_heads)
         self.mlp = nn.Sequential(
             nn.Linear(dim, dim * 4), nn.GELU(), nn.Linear(dim * 4, dim)
         )
         self.norm1 = nn.LayerNorm(dim)
         self.norm2 = nn.LayerNorm(dim)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.norm1(x + self.msa(x))
         x = self.norm2(x + self.mlp(x))
         return x
 
 
 class Transformer(nn.Module):
-    def __init__(self, input_dim, dim, n_heads, n_blocks, n_classes):
+    def __init__(
+        self, input_dim: int, dim: int, n_heads: int, n_blocks: int, n_classes: int
+    ):
         super().__init__()
         # store parameters
         self.input_dim = input_dim
@@ -73,22 +92,21 @@ class Transformer(nn.Module):
         self.n_classes = n_classes
 
         # prepare position encoding
-        self.positional_encoding = None
+        self.positional_encoding = torch.empty((0, 0))
 
         # prepare layers
         self.embedding = nn.Linear(input_dim, dim)
         self.blocks = nn.Sequential(
-            *[TransformerBlock(n_heads, dim) for i in range(n_blocks)]
+            *[TransformerBlock(dim, n_heads) for i in range(n_blocks)]
         )
         self.classification_head = nn.Linear(dim, n_classes)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         # generate position encoding if necessary
-        if (
-            self.positional_encoding is None
-            or self.positional_encoding.shape[0] < x.shape[1]
-        ):
-            self.positional_encoding = positional_encoding(x.shape[1], self.dim).to(x.device)
+        if self.positional_encoding.shape[0] < x.shape[1]:
+            self.positional_encoding = positional_encoding(x.shape[1], self.dim).to(
+                x.device
+            )
 
         # embedd input and add position encoding
         x = self.embedding(x) + self.positional_encoding
