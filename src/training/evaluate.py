c65e65a67534e09ac15793f73d19c8194f5fccb0
Jan Karwowski <j.karwowski@pixel.com.pl>
2023-01-21 12:57:59 +0100

    Fix validation in supervised training + evaluate on best model

--- src/training/evaluate.py
+++ src/training/evaluate.py
@@ -24,6 +24,28 @@ from src.annotation_parser.chord_model import LabelOccurence, ChordOccurence, cs
 from src.annotation_parser.labfile_printer import print_labfile
 
 
+def partial_predict(model: torch.nn.Module, audio: torch.Tensor, frames_per_item: int):
+    n_frames, n_features = audio.shape
+    predictions = torch.zeros(size=(n_frames,), dtype=torch.int, device=audio.device)
+    for i in range(0, n_frames, frames_per_item // 2):
+        # correct to end of song for last item(s)
+        if i + frames_per_item > n_frames:
+            i = n_frames - frames_per_item
+        # extract frames of single item
+        audio_item = audio[i: i + frames_per_item]
+        # predict labels
+        with torch.no_grad():
+            prediction = torch.argmax(model(audio_item.unsqueeze(0)), dim=2)[0]
+        # fill all predictions with new predictions
+        if i > 0:
+            predictions[
+                i + (frames_per_item // 4): i + frames_per_item
+            ] = prediction[frames_per_item // 4:]
+        else:
+            predictions[i: i + frames_per_item] = prediction
+    return predictions
+
+
 def evaluate(
     dataset: SongDataset,
     model: Transformer,
@@ -43,108 +65,92 @@ def evaluate(
 
     output_dir = f"{output_dir_prefix}_{datetime.now().strftime('%Y%m%d%H%M%S')}"
 
-    with torch.no_grad():
-        all_csrs = []
-        all_n_frames = []
-        all_predictions = []
-        all_labels = []
-        for item_index in tqdm(range(len(dataset)), total=len(dataset), unit="item"):
-            audio, labels = dataset[item_index]
-            audio, labels = torch.tensor(audio).cuda(), torch.tensor(labels).cuda()
-            song_metadata = dataset.get_song_metadata(item_index)
+    all_csrs = []
+    all_n_frames = []
+    all_predictions = []
+    all_labels = []
+    for item_index in tqdm(range(len(dataset)), total=len(dataset), unit="item"):
+        audio, labels = dataset[item_index]
+        audio, labels = torch.tensor(audio).cuda(), torch.tensor(labels).cuda()
+        song_metadata = dataset.get_song_metadata(item_index)
 
-            # get model predictions for song
-            predictions = torch.zeros_like(labels)
-            for i in range(0, audio.shape[0], frames_per_item // 2):
-                # correct to end of song for last item(s)
-                if i + frames_per_item > audio.shape[0]:
-                    i = audio.shape[0] - frames_per_item
-                # extract frames of single item
-                item_audio = audio[i: i + frames_per_item]
-                # predict labels
-                prediction = torch.argmax(model(item_audio.unsqueeze(0)), dim=2)[0]
-                # fill all predictions with new predictions
-                if i > 0:
-                    predictions[
-                        i + (frames_per_item // 4): i + frames_per_item
-                    ] = prediction[frames_per_item // 4:]
-                else:
-                    predictions[i: i + frames_per_item] = prediction
+        # get model predictions for song
+        predictions = partial_predict(model, audio, frames_per_item)
 
-            # predictions and labels to numpy
-            predictions = predictions.cpu().numpy()
-            labels = labels.cpu().numpy()
+        # convert to numpy
+        predictions = predictions.cpu().numpy()
+        labels = labels.cpu().numpy()
 
-            # convert predictions to label occurences and merge repeated
-            start_times = np.arange(len(predictions)) * (
-                dataset.config.hop_size / dataset.config.sample_rate
-            )
-            stop_times = start_times + (dataset.config.frame_size / dataset.config.sample_rate)
-            pred_label_occurences: List[LabelOccurence] = []
-            for start, stop, label in zip(start_times, stop_times, predictions):
-                if (
-                    len(pred_label_occurences) > 0
-                    and pred_label_occurences[-1].label == label
-                ):
-                    pred_label_occurences[-1] = replace(
-                        pred_label_occurences[-1], stop=stop
-                    )
-                else:
-                    pred_label_occurences.append(LabelOccurence(start, stop, label))
+        # convert predictions to label occurences and merge repeated
+        start_times = np.arange(len(predictions)) * (
+            dataset.config.hop_size / dataset.config.sample_rate
+        )
+        stop_times = start_times + (dataset.config.frame_size / dataset.config.sample_rate)
+        pred_label_occurences: List[LabelOccurence] = []
+        for start, stop, label in zip(start_times, stop_times, predictions):
+            if (
+                len(pred_label_occurences) > 0
+                and pred_label_occurences[-1].label == label
+            ):
+                pred_label_occurences[-1] = replace(
+                    pred_label_occurences[-1], stop=stop
+                )
+            else:
+                pred_label_occurences.append(LabelOccurence(start, stop, label))
 
-            with tempfile.TemporaryDirectory() as tmp_dir:
-                # create labfile
-                with open(os.path.join(tmp_dir, "predictions.lab.txt"), "w") as f:
-                    f.write(
-                        print_labfile(
-                            [
-                                label_occurence.to_chord_occurence(
-                                    dataset.config.labels_vocabulary
-                                )
-                                for label_occurence in pred_label_occurences
-                            ]
-                        ),
-                    )
-
-                # calculate CSR and other classification ("framewise") metrics for this song
-                metrics = {
-                    "csr": csr(
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            # create labfile
+            with open(os.path.join(tmp_dir, "predictions.lab.txt"), "w") as f:
+                f.write(
+                    print_labfile(
                         [
-                            chord_occurence.to_label_occurence(
+                            label_occurence.to_chord_occurence(
                                 dataset.config.labels_vocabulary
                             )
-                            for chord_occurence in parse_annotation_file(
-                                song_metadata.filepath
-                            )
-                        ],
-                        pred_label_occurences,
+                            for label_occurence in pred_label_occurences
+                        ]
                     ),
-                    "accuracy": accuracy_score(labels, predictions),
-                    "recall": list(
-                        recall_score(labels, predictions, **recall_precision_kwargs)
-                    ),
-                    "precision": list(
-                        precision_score(labels, predictions, **recall_precision_kwargs)
-                    ),
-                }
-                with open(os.path.join(tmp_dir, "metrics.json"), "w") as f:
-                    json.dump(metrics, f)
-
-                # plot confusion matrix
-                ConfusionMatrixDisplay.from_predictions(
-                    labels, predictions, labels=recall_precision_kwargs["labels"]
                 )
-                plt.savefig(os.path.join(tmp_dir, "confusion_matrix.png"), dpi=200)
-                plt.close()
 
-                # log to mlflow
-                mlflow.log_artifacts(tmp_dir, f"{output_dir}/{song_metadata.name}")
+            # calculate CSR and other classification ("framewise") metrics for this song
+            metrics = {
+                "csr": csr(
+                    [
+                        chord_occurence.to_label_occurence(
+                            dataset.config.labels_vocabulary
+                        )
+                        for chord_occurence in parse_annotation_file(
+                            song_metadata.filepath
+                        )
+                    ],
+                    pred_label_occurences,
+                ),
+                "accuracy": accuracy_score(labels, predictions),
+                "recall": list(
+                    recall_score(labels, predictions, **recall_precision_kwargs)
+                ),
+                "precision": list(
+                    precision_score(labels, predictions, **recall_precision_kwargs)
+                ),
+            }
+            with open(os.path.join(tmp_dir, "metrics.json"), "w") as f:
+                json.dump(metrics, f)
 
-            # store values for global metrics calculation
-            all_csrs.append(metrics["csr"])
-            all_n_frames.append(len(labels))
-            all_labels.append(labels)
-            all_predictions.append(predictions)
+            # plot confusion matrix
+            ConfusionMatrixDisplay.from_predictions(
+                labels, predictions, labels=recall_precision_kwargs["labels"]
+            )
+            plt.savefig(os.path.join(tmp_dir, "confusion_matrix.png"), dpi=200)
+            plt.close()
+
+            # log to mlflow
+            mlflow.log_artifacts(tmp_dir, f"{output_dir}/{song_metadata.name}")
+
+        # store values for global metrics calculation
+        all_csrs.append(metrics["csr"])
+        all_n_frames.append(len(labels))
+        all_labels.append(labels)
+        all_predictions.append(predictions)
 
     # calculate WCSR and classification metrics for all songs
     labels = np.concatenate(all_labels)
--- src/training/training.py
+++ src/training/training.py
@@ -13,7 +13,7 @@ from torchinfo import summary
 
 from src.training.dataset import SongDataset, SongDatasetConfig, song_dataset_collate_fn
 from src.training.model import Transformer
-from src.training.evaluate import evaluate
+from src.training.evaluate import evaluate, partial_predict
 
 
 def is_rank_0():
@@ -92,6 +92,9 @@ def train(args):
 
     # init datasets and data loaders
     song_dataset_config = SongDatasetConfig.create_from_args(args)
+    song_eval_dataset_config = replace(
+        song_dataset_config, frames_per_item=0, pitch_shift_augment=False, song_multiplier=1, fraction=None
+    )
     train_ds = SongDataset(["train"], song_dataset_config)
     train_dl = DataLoader(
         train_ds,
@@ -101,15 +104,7 @@ def train(args):
         collate_fn=song_dataset_collate_fn,
         worker_init_fn=worker_init_fn,
     )
-    validate_ds = SongDataset(["validate"], replace(song_dataset_config, pitch_shift_augment=False, fraction=None))
-    validate_dl = DataLoader(
-        validate_ds,
-        batch_size=args.batch_size,
-        num_workers=5,
-        sampler=DistributedSampler(validate_ds) if args.ddp else None,
-        collate_fn=song_dataset_collate_fn,
-        worker_init_fn=worker_init_fn
-    )
+    validate_ds = SongDataset(["validate"], song_eval_dataset_config)
 
     # prepare model and optimizer
     model = Transformer(
@@ -136,11 +131,15 @@ def train(args):
     validate_accuracy = Accuracy().cuda()
     loss_metric = MeanMetric().cuda()
 
+    # prepare best state variables
+    best_validate_epoch = 0
+    best_validate_accuracy = 0.0
+    best_model_state = model.state_dict()
+
     # training loop
     for epoch in tqdm(range(args.n_epochs), unit="epoch"):
         if args.ddp:
             train_dl.sampler.set_epoch(epoch)
-            validate_dl.sampler.set_epoch(epoch)
 
         # train
         model.train()
@@ -169,37 +168,40 @@ def train(args):
         log_metric("train / epoch / accuracy", train_accuracy.compute(), epoch)
 
         # validate
-        model.eval()
-        validate_accuracy.reset()
-        for audio, labels in tqdm(validate_dl, total=len(validate_dl), unit="batch"):
-            with torch.no_grad():
-                audio, labels = audio.cuda(), labels.cuda()
-                validate_accuracy(
-                    rearrange(model(audio), "b s c-> (b s) c"),
-                    rearrange(labels, "b s -> (b s)"),
-                )
-        log_metric("validate / epoch / accuracy", validate_accuracy.compute(), epoch)
+        if is_rank_0():
+            model.eval()
+            validate_accuracy.reset()
+            for song_index in tqdm(range(len(validate_ds)), total=len(validate_ds), unit="song"):
+                audio, labels = validate_ds[song_index]
+                audio, labels = torch.tensor(audio).cuda(), torch.tensor(labels).cuda()
+                prediction = partial_predict(model, audio, args.frames_per_item)
+                validate_accuracy(prediction, labels)
+            acc = validate_accuracy.compute().item()
+            if acc >= best_validate_accuracy:
+                best_validate_epoch = epoch
+                best_validate_accuracy = acc
+                best_model_state = model.state_dict()
+            log_metric("validate / epoch / accuracy", acc, epoch)
+            log_metric("validate / epoch / best_epoch", best_validate_epoch, epoch)
 
     # evaluate model
     if is_rank_0():
+        model.load_state_dict(best_model_state)
         model.eval()
-        song_dataset_config_eval = replace(
-            song_dataset_config, frames_per_item=0, pitch_shift_augment=False, song_multiplier=1, fraction=None
-        )
         evaluate(
-            SongDataset(["train"], song_dataset_config_eval),
+            SongDataset(["train"], song_eval_dataset_config),
             model,
             "train_ds_evaluation",
             args.frames_per_item,
         )
         evaluate(
-            SongDataset(["validate"], song_dataset_config_eval),
+            SongDataset(["validate"], song_eval_dataset_config),
             model,
             "validate_ds_evaluation",
             args.frames_per_item,
         )
         evaluate(
-            SongDataset(["test"], song_dataset_config_eval),
+            SongDataset(["test"], song_eval_dataset_config),
             model,
             "test_ds_evaluation",
             args.frames_per_item,
