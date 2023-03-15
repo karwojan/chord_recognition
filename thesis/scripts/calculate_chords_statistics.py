ff435e3b36ae71d3986af8f22ea1014dd7a10147
Jan Karwowski <j.karwowski@pixel.com.pl>
2023-03-15 15:20:54 +0100

    Fixes in dataset chapter

--- thesis/scripts/calculate_chords_statistics.py
+++ thesis/scripts/calculate_chords_statistics.py
@@ -9,7 +9,7 @@ from src.annotation_parser import parse_annotation_file
 
 # read index
 index = pd.read_csv("data/index.csv", sep=";")
-index = index.query("purpose == 'train' or purpose == 'test' or purpose == 'validate'")
+index = index.query(" or ".join(f"purpose == 'train_fold_{i}'" for i in range(5)))
 
 # calculate statistics
 statistics: Dict[int, float] = {}
@@ -24,7 +24,7 @@ plt.xticks(np.arange(25), rotation=90, labels=[
     "C:maj", "C#:maj", "D:maj", "D#:maj", "E:maj", "F:maj", "F#:maj", "G:maj", "G#:maj", "A:maj", "A#:maj", "B:maj",
     "C:min", "C#:min", "D:min", "D#:min", "E:min", "F:min", "F#:min", "G:min", "G#:min", "A:min", "A#:min", "B:min"
 ])
-plt.xlabel("Nazw akordu")
+plt.xlabel("Nazwa akordu")
 plt.ylabel("Łączny czas występowania [h]")
 plt.bar(statistics.keys(), statistics.values())
 plt.show()
